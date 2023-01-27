6bd60ed15554b4841aff1b269eca3e195642128c
danielhe4rt <hey@danielheart.dev>
2023-01-27 19:07:16 -0300

    feat: base seeder

---
+++ config/ecommerce.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+    'categories' => [
+        'celulares',
+        'informática',
+        'móveis',
+        'automotiva',
+        'livros',
+        'beleza',
+        'esporte',
+        'luxo'
+    ]
+];
